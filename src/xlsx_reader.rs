3dd7453b922e13692cce8f03b3db49795febe8df
tn_zhuxg <1134495285@qq.com>
2026-02-21 23:41:06 +0800

    feat: enable simd

--- src/xlsx_reader.rs
+++ src/xlsx_reader.rs
@@ -1,31 +1,21 @@
-use calamine::{Reader, Xlsx, open_workbook_from_rs};
+use calamine::{Range, Data, Reader, Xlsx, open_workbook_from_rs};
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
 pub struct XlsxInfo {
-    table_names: Vec<String>,
+    sheets: Vec<(String, Range<Data>)>
 }
 
-#[wasm_bindgen]
 impl XlsxInfo {
-    #[wasm_bindgen(constructor)]
-    pub fn new(table_names: Vec<String>) -> XlsxInfo {
-        XlsxInfo { table_names }
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn table_names(&self) -> Vec<String> {
-        self.table_names.clone()
+    pub fn new(sheets: Vec<(String, Range<Data>)>) -> XlsxInfo {
+        XlsxInfo { sheets }
     }
 }
 
-#[wasm_bindgen]
 pub fn read_xlsx(data: &[u8]) -> Result<XlsxInfo, JsError> {
     let cursor = Cursor::new(data);
-    let reader: Xlsx<_> =
+    let mut reader: Xlsx<_> =
         open_workbook_from_rs(cursor)?;
-    let table_names = reader.sheet_names();
 
-    Ok(XlsxInfo::new(table_names))
+    Ok(XlsxInfo::new(reader.worksheets()))
 }
