f15dba7556db0be77280a6daca6cda82f1826947
tn_zhuxg <1134495285@qq.com>
2026-02-23 23:11:41 +0800

    feat: add wasm-opt

--- build-mp.sh
+++ build-mp.sh
@@ -4,6 +4,15 @@ rm -rf ./dist-mp/*
 
 cargo +nightly build --release
 
+wasm-opt -O3 \
+    --enable-nontrapping-float-to-int \
+    --enable-bulk-memory \
+    --enable-simd \
+    --strip-debug \
+    --strip-producers \
+    target/wasm32-unknown-unknown/release/xlsx_reader.wasm \
+    -o target/wasm32-unknown-unknown/release/xlsx_reader.wasm
+
 wasm-bindgen \
     target/wasm32-unknown-unknown/release/xlsx_reader.wasm \
     --out-dir dist-mp \
--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 cargo +nightly build --release
 
+wasm-opt -O3 \
+    --enable-nontrapping-float-to-int \
+    --enable-bulk-memory \
+    --enable-simd \
+    --strip-debug \
+    --strip-producers \
+    target/wasm32-unknown-unknown/release/xlsx_reader.wasm \
+    -o target/wasm32-unknown-unknown/release/xlsx_reader.wasm
+
 wasm-bindgen \
     target/wasm32-unknown-unknown/release/xlsx_reader.wasm \
     --out-dir ./web/src/xlsx-reader \
